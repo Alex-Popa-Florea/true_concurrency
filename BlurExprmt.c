2e01219918e206d72c07268d75611cbdbb79ae77
Alexander Popa Florea <alexander.popa-florea20@imperial.ac.uk>
2021-12-17 12:51:05 +0000

    Implement thread pool pixel by pixel version

--- BlurExprmt.c
+++ BlurExprmt.c
@@ -7,12 +7,55 @@
 #include <time.h>
 #include <math.h>
 #include "BlurExprmt.h"
+#include "thpool/thpool.h"
 
 #define BLUR_REGION_SIZE 9
 #define BILLION  1000000000L
-#define THREADLIMIT 4
-#define IMPLEMENTATIONS 6
+
+#define IMPLEMENTATIONS 7
+#define THREADLIMIT 100
+
 // ---------- MAIN PROGRAM ---------- \\
+
+  static void *blur_chunk(void *);
+  static void blur_chunk_thpool(void *);
+  static void *do_tasks(void *);
+  static void blur_column_by_column(struct picture *);
+  static void blur_row_by_row(struct picture *);
+  static void blur_pixel_by_pixel_with_task_stack(struct picture *);
+  static void blur_pixel_by_pixel_with_thpool(struct picture *);
+  static void blur_sector_by_sector(struct picture *, int);
+
+  // Array storing file names
+  char *files_names[IMPLEMENTATIONS] = {
+    "experiment_images/base_blur.jpg", 
+    "experiment_images/row_by_row_blur.jpg", 
+    "experiment_images/column_by_column_blur.jpg", 
+    "experiment_images/pixel_by_pixel_stack_blur.jpg",
+    "experiment_images/pixel_by_pixel_thpool_blur.jpg", 
+    "experiment_images/few_sector_by_sector_blur.jpg", 
+    "experiment_images/many_sector_by_sector_blur.jpg"
+  };
+  // Array storing implementation names
+  char *implemenation_name[IMPLEMENTATIONS] = {
+    "Sequential", 
+    "Row by Row", 
+    "Column by Column", 
+    "Pixel by Pixel using stack of tasks", 
+    "Pixel by Pixel using thread pool", 
+    "Sector by Sector with 4 sectors", 
+    "Sector by Sector with 100 sectors" 
+  };
+  // Array storing the function pointers
+  blur_func functions[IMPLEMENTATIONS] = { 
+    &blur_picture, 
+    &blur_row_by_row, 
+    &blur_column_by_column, 
+    &blur_pixel_by_pixel_with_task_stack, 
+    &blur_pixel_by_pixel_with_thpool, 
+    &blur_sector_by_sector, 
+    &blur_sector_by_sector 
+  };
 
 
   /*
@@ -30,9 +73,6 @@
   */
   struct task_args *list_pop(list_t *list) {
     list_elem_t *old_head = list->head;
-    if (old_head == NULL) {
-      return NULL;
-    }
     struct task_args *task = old_head->task;
     list->head = old_head->next;
     free(old_head);
@@ -44,8 +84,9 @@
     Funcition to push to the stack;
   */
   void list_push(list_t *list, struct task_args *task) {
-    list_elem_t *new_head = malloc(sizeof(list_elem_t));
+    list_elem_t *new_head = (list_elem_t *) malloc(sizeof(list_elem_t));
     if (new_head == NULL) {
+      printf("Ran out of memory for malloc!\n");
       return;
     }
     new_head->next = list->head;
@@ -57,7 +98,7 @@
   /*
     Generic bluring function to pass to the threads with the args.
   */
-  void *blur_chunk(void *args_ptr){
+  static void *blur_chunk(void *args_ptr){
     struct task_args *args = (struct task_args *) args_ptr;
     for(int i = args->i_start; i < args->i_end + 1; i++){
       for(int j = args->j_start; j < args->j_end + 1; j++){
@@ -84,20 +125,22 @@
     }
   }
 
+  static void blur_chunk_thpool(void *args_ptr){
+    blur_chunk(args_ptr);
+  }
+
   /*
     Function to give to blur pixel which uses the stack to reuse threads
     After bluring one pixel, it pops another from the list to blur another one
     until the task stack is emptied.
   */
-  void *do_tasks() {
+  static void *do_tasks(void *UNUSED) {
     pthread_mutex_lock(&task_list_lock);
     while(task_list.size != 0) {
       struct task_args *task = list_pop(&task_list);
-      if (task == NULL) {
-        break;
-      }
       pthread_mutex_unlock(&task_list_lock);
       blur_chunk(task);
+      free(task);
       pthread_mutex_lock(&task_list_lock);
     }
     pthread_mutex_unlock(&task_list_lock);
@@ -106,13 +149,21 @@
   /*
     Bluring function that does bluring in a column by column manner.
   */
-  void blur_column_by_column(struct picture *pic){
+  static void blur_column_by_column(struct picture *pic){
     struct picture tmp;
     tmp.img = copy_image(pic->img);
     tmp.width = pic->width;
     tmp.height = pic->height;  
-    pthread_t *threads = malloc((tmp.width - 2) * sizeof(pthread_t));
-    struct task_args *args = malloc((tmp.width - 2) * sizeof(struct task_args));
+    pthread_t *threads = (pthread_t *) malloc((tmp.width - 2) * sizeof(pthread_t));
+    if (threads == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return;
+    }
+    struct task_args *args = (struct task_args *) malloc((tmp.width - 2) * sizeof(struct task_args));
+    if (args == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return;
+    }
     for(int i = 1 ; i < tmp.width - 1; i++){
       args[i - 1].pic = pic;
       args[i - 1].tmp = tmp;
@@ -135,13 +186,21 @@
   /*
     Bluring function that does bluring in a row by row manner.
   */
-  void blur_row_by_row(struct picture *pic){
+  static void blur_row_by_row(struct picture *pic){
     struct picture tmp;
     tmp.img = copy_image(pic->img);
     tmp.width = pic->width;
     tmp.height = pic->height;  
-    pthread_t *threads = malloc((tmp.height - 2) * sizeof(pthread_t));
-    struct task_args *args = malloc((tmp.height - 2) * sizeof(struct task_args));
+    pthread_t *threads = (pthread_t *) malloc((tmp.height - 2) * sizeof(pthread_t));
+    if (threads == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return;
+    }
+    struct task_args *args = (struct task_args *) malloc((tmp.height - 2) * sizeof(struct task_args));
+    if (args == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return;
+    }
     for(int j = 1 ; j < tmp.height - 1; j++){
       args[j - 1].pic = pic;
       args[j - 1].tmp = tmp;
@@ -162,18 +221,28 @@
   }
 
   /*
-    Bluring function that does bluring in a pixel by pixel manner.
+    Bluring function that does bluring in a pixel by pixel manner using a stack
+    of tasks.
   */
-  void blur_pixel_by_pixel(struct picture *pic){
+  static void blur_pixel_by_pixel_with_task_stack(struct picture *pic){
+    task_list.head = NULL;
+    task_list.size = 0;
     struct picture tmp;
     tmp.img = copy_image(pic->img);
     tmp.width = pic->width;
     tmp.height = pic->height;  
-    pthread_t *threads = malloc(THREADLIMIT * sizeof(pthread_t));
-
+    pthread_t *threads = (pthread_t *) malloc(THREADLIMIT * sizeof(pthread_t));
+    if (threads == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return;
+    }
     for(int i = 1; i < tmp.width - 1; i++){
       for(int j = 1; j < tmp.height - 1; j++){
-        struct task_args *task = malloc(sizeof(struct task_args));
+        struct task_args *task = (struct task_args *) malloc(sizeof(struct task_args));
+        if (task == NULL) {
+          printf("Ran out of memory for malloc!\n");
+          return;
+        }
         task->pic = pic;
         task->tmp = tmp;
         task->i_start = i;
@@ -196,6 +265,43 @@
   }
 
   /*
+    Bluring function that does bluring in a pixel by pixel manner using a thread
+    pool.
+  */
+  static void blur_pixel_by_pixel_with_thpool(struct picture *pic){
+    struct picture tmp;
+    tmp.img = copy_image(pic->img);
+    tmp.width = pic->width;
+    tmp.height = pic->height;
+    struct task_args *args = (struct task_args *) malloc((tmp.width - 2) * (tmp.height - 2) * sizeof(struct task_args));
+    if (args == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return;
+    }
+    threadpool thread_pool = thpool_init(THREADLIMIT);
+    int index = 0;
+    for(int i = 1; i < tmp.width - 1; i++){
+      for(int j = 1; j < tmp.height - 1; j++){
+        args[index].pic = pic;
+        args[index].tmp = tmp;
+        args[index].i_start = i;
+        args[index].i_end = i;
+        args[index].j_start = j;
+        args[index].j_end = j;
+
+        thpool_add_work(thread_pool, blur_chunk_thpool, &args[index]);
+
+        index++;
+      }
+    }
+    thpool_wait(thread_pool);
+    thpool_destroy(thread_pool);
+
+    clear_picture(&tmp);
+    free(args);
+  }
+
+  /*
     Bluring function that does bluring in a sector by sector manner.
     The number of sectors is given in the sectors parameter, which has to
     be a square number, as the image will be split into equal rectangles.
@@ -203,18 +309,20 @@
     size than the rest if the image width and height are not divisible by the
     root of the number of sectors.
   */
-  void blur_sector_by_sector(struct picture *pic, int sectors){
+  static void blur_sector_by_sector(struct picture *pic, int sectors){
     int split = sqrt(sectors);
     struct picture tmp;
     tmp.img = copy_image(pic->img);
     tmp.width = pic->width;
     tmp.height = pic->height;
-    pthread_t *threads = malloc(split * split * sizeof(pthread_t));
+    pthread_t *threads = (pthread_t *) malloc(split * split * sizeof(pthread_t));
     if (threads == NULL) {
+      printf("Ran out of memory for malloc!\n");
       return;
     }
-    struct task_args *args = malloc(split * split * sizeof(struct task_args));
+    struct task_args *args = (struct task_args *) malloc(split * split * sizeof(struct task_args));
     if (args == NULL) {
+      printf("Ran out of memory for malloc!\n");
       return;
     }
 
@@ -299,9 +407,9 @@
     blur_picture. (This is done as the first blur in the experiment, 
     and is assumbed correct)
   */
-  bool check_correctness(char *file) {
+  static bool check_correctness(char *file) {
     char command[500];
-    sprintf(command, "./picture_compare base_blur.jpg %s", file);
+    sprintf(command, "./picture_compare %s %s", files_names[0], file);
     if (system(command) != 0) {
       printf("\nPicture %s was not blurred correctly!\n", file);
       return false;
@@ -313,24 +421,34 @@
 
   int main(int argc, char **argv){
 
-    struct picture *pic = malloc(sizeof(struct picture));
+    struct picture *original = (struct picture *) malloc(sizeof(struct picture));
+    if (original == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return EXIT_FAILURE;
+    }
+    init_picture_from_file(original, argv[1]);
+    struct picture *pic = (struct picture *) malloc(sizeof(struct picture));
+    if (pic == NULL) {
+      printf("Ran out of memory for malloc!\n");
+      return EXIT_FAILURE;
+    }
+    pic->width = original->width;
+    pic->height = original->height;
+
     struct timespec start;
     struct timespec end;
     double diff;
     double sum = 0;
     bool correctness = true;
-    double average[IMPLEMENTATIONS];
-    char *files_names[IMPLEMENTATIONS] = {"base_blur.jpg", "row_by_row_blur.jpg", "column_by_column_blur.jpg", "pixel_by_pixel_blur.jpg", "few_sector_by_sector_blur.jpg", "many_sector_by_sector_blur.jpg"};
-    char *implemenation_name[IMPLEMENTATIONS] = {"Sequential", "Row by Row", "Column by Column", "Pixel by Pixel", "Sector by Sector with 100 sectors", "Sector by Sector with 4 sectors" };
-    blur_func functions[IMPLEMENTATIONS] = { &blur_picture, &blur_row_by_row, &blur_column_by_column, &blur_pixel_by_pixel, &blur_sector_by_sector, &blur_sector_by_sector };
-    
+    double average[IMPLEMENTATIONS] = {0, 0, 0, 0, 0, 0};
+
     int repeats = atoi(argv[3]);
 
     // Creates a file to store the results of the experiment.
     FILE *f = fopen(argv[2], "w");
     if (f == NULL){
-        printf("Error opening file!\n");
-        return -1;
+      printf("Error opening file!\n");
+      return EXIT_FAILURE;
     }
 
     printf("\nBegining the Blur Experiment: \n");
@@ -341,15 +459,17 @@
       printf("\n%s Implementation: \n\n", implemenation_name[r]);
       fprintf(f, "\n%s Implementation: \n\n", implemenation_name[r]);
       for (int i = 0; i < repeats; i++){
-        init_picture_from_file(pic, argv[1]);
-        
+        // Copies the image to ensure the bluring occurs on the same one each time
+        pic->img = copy_image(original->img);
+        // Measure time using CLOCK_REALTIME
         clock_gettime(CLOCK_REALTIME, &start);
+        // Choose which function to use
         switch (r){
-          case (IMPLEMENTATIONS - 1):
+          case 5:
             functions[r] (pic, 4);
             break;
 
-          case IMPLEMENTATIONS:
+          case 6:
             functions[r] (pic, 100);
             break;
 
@@ -360,15 +480,17 @@
         clock_gettime(CLOCK_REALTIME, &end);
 
         save_picture_to_file(pic, files_names[r]);
-
+        // Checks the correctness of the blurred files
         if (r != 0) {
-          correctness &= check_correctness("column_by_column_blur.jpg");
+          correctness &= check_correctness(files_names[r]);
         }
-
+        // Compute the time of the iteration
         diff = ((double) (end.tv_sec - start.tv_sec)) + ((double) (end.tv_nsec - start.tv_nsec)) / BILLION;
         fprintf(f, "Time in iteration %d for %s: %lf seconds\n", i, implemenation_name[r], diff);
         sum += diff;
+        clear_picture(pic);
       }
+      // Computes the average time of all iterations.
       average[r] = sum / repeats;
       sum = 0;
       printf("Average time for %s: %lf seconds\n\n", implemenation_name[r], average[r]);
@@ -393,9 +515,12 @@
       printf ("\nNot all images were blurred correctly.\n");
     }
 
-    clear_picture(pic);
     free(pic);
+
+    clear_picture(original);
+    free(original);
+
     fclose(f);
 
-    return 0;
+    return EXIT_SUCCESS;
   }
